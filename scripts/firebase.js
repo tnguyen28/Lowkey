3f5a7170e96649892cd5db0f797a12fdb45c96c5
christopherjose <cjose@luc.edu>
2019-12-03 14:39:08 -0600

    firebase file

---
+++ scripts/firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.5.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAao9IgOAN0vArAsUF5YxnaCUn-4gilP_U",
+    authDomain: "lowkey-61411.firebaseapp.com",
+    databaseURL: "https://lowkey-61411.firebaseio.com",
+    projectId: "lowkey-61411",
+    storageBucket: "lowkey-61411.appspot.com",
+    messagingSenderId: "378529906556",
+    appId: "1:378529906556:web:13bf69e23bd07de1a92ef9",
+    measurementId: "G-NFBVECCQQ3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
